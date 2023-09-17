032c0789bcdb202f515ebef9b64ed4f96c2cdc26
frr <fryderyk32j@gmail.com>
2023-09-17 11:47:50 +0200

    Added new GET endpoint

--- src/main/java/com/example/JenkinsSpringWebApplication.java
+++ src/main/java/com/example/JenkinsSpringWebApplication.java
@@ -2,12 +2,27 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JenkinsSpringWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsSpringWebApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world";
+	}
+
+	@GetMapping("/{name}")
+	public String helloName(@PathVariable String name) {
+		return "Hello " + name;
 	}
 
 }
--- src/test/java/com/example/JenkinsSpringWebApplicationTests.java
+++ src/test/java/com/example/JenkinsSpringWebApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JenkinsSpringWebApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Should return \"Hello world\" when GET request on \"/\"")
+	void shouldReturnHelloWorldWhenGetRequest() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello world"));
+	}
+
+	@Test
+	@DisplayName("Should return \"Hello Michael\" when GET request on \"/\"")
+	void shouldReturnHelloMichaelWhenGetRequest() throws Exception {
+		String name = "Michael";
+		mockMvc.perform(MockMvcRequestBuilders.get("/" + name))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello Michael"));
 	}
 
 }
